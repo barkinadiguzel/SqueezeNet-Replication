342c35e3d1ecfa8d0fe75ce5e7663b49e6923828
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-11-23 21:12:16 +0300

    Create fire_module.py

---
+++ src/layers/fire_module.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class FireModule(nn.Module):
+    def __init__(self, in_channels, squeeze_channels, expand1x1_channels, expand3x3_channels):
+        super().__init__()
+        self.squeeze = nn.Conv2d(in_channels, squeeze_channels, kernel_size=1)
+        self.squeeze_relu = nn.ReLU(inplace=True)
+        
+        self.expand1x1 = nn.Conv2d(squeeze_channels, expand1x1_channels, kernel_size=1)
+        self.expand3x3 = nn.Conv2d(squeeze_channels, expand3x3_channels, kernel_size=3, padding=1)
+        self.expand_relu = nn.ReLU(inplace=True)
+        
+    def forward(self, x):
+        x = self.squeeze_relu(self.squeeze(x))
+        return self.expand_relu(torch.cat([
+            self.expand1x1(x),
+            self.expand3x3(x)
+        ], dim=1))
